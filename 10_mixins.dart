b40b21866f635a00cbf319bf047841d415190b96
JChar <88268441+JeivCH@users.noreply.github.com>
2024-01-28 19:40:19 +0000

    Update 10_mixins.dart
    cambiado el modificador abstract por mixin, para poder utilizar el nuevo modo de dart

--- 10_mixins.dart
+++ 10_mixins.dart
@@ -1,52 +1,54 @@
-abstract class Animal {}
+abstract class Animal{}
 
-abstract class Mamifero extends Animal {}
-abstract class Ave extends Animal {}
-abstract class Pez extends Animal {}
 
-abstract class Volador {
-  void volar() => print('estoy volando!');
+//solo habra caracteristicas de mamiferos espeficicas 
+abstract class Mamifero extends Animal{}
+
+abstract class Ave extends Animal{}
+// solo cosas que tengan que ver con peces
+abstract class Pez extends Animal{}
+
+
+mixin Volador{
+  void volar() =>print('estoy volando!');
 }
 
-abstract class Caminante {
-  void caminar() => print('estoy caminando!');
+mixin Caminante{
+  void caminar() =>print('estoy andando!');
 }
 
-abstract class Nadador {
-  void nadar() => print('estoy nadando!');
+mixin Nadador{
+  void nadar() =>print('estoy nadando!');
 }
 
 
 class Delfin extends Mamifero with Nadador {}
 class Murcielago extends Mamifero with Volador, Caminante {}
-class Gato extends Mamifero with Caminante {}
 
-class Paloma extends Ave with Caminante, Volador {}
-class Pato extends Ave with Caminante, Volador, Nadador {}
+class Gato extends Mamifero with Caminante{}
 
-class Tiburon extends Pez with Nadador {}
-class PezVolador extends Pez with Nadador, Volador {}
+class Paloma extends Ave with Volador, Caminante{}
 
+class Pato extends Ave with Volador, Nadador,Caminante{}
+
+class Tiburon extends Pez with Nadador{}
+
+class PezVolador extends Pez with Volador, Nadador{}
 
 void main() {
   
-  final flipper = Delfin();
+ final flipper = Delfin();
   flipper.nadar();
   
-  final batman = Murcielago();
-  batman.caminar();
-  batman.volar();
+  final seth = Gato();
+  seth.caminar();
+  
+  final donald = Pato();
+  donald.caminar();
+  donald.nadar();
   
   
-  final namor = Pato();
-  namor.caminar();
-  namor.volar();
-  namor.nadar();
-   
 }
-
-
-
 
 
 
